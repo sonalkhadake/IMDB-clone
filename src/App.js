9cc8024e4ce87909b9abbe3bdad2282c84ff55f2
sonali <sonalkhadake@gmail.com>
2023-03-27 22:16:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/header/Header';
+import Home from './screens/home/Home';
+import MovieList from './components/movieList/MovieList';
+import Movie from './screens/MovieDetails/Movie';
+// 8a432e8db3c833b79e44bd2c6dce6538
+//https://github.com/PrinceRaaaj/IMDB-clone-ReactApp
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className="App">
+   <Router>
+    <Header/>
+      <Routes>
+      <Route index element={<Home/>}></Route>
+                <Route path="movie/:id" element={<Movie/>}></Route>
+                <Route path="movies/:type" element={<MovieList/>}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+      </Routes>
+    </Router>
+ </div>
+   
+    
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/2560px-YouTube_full-color_icon_%282017%29.svg.png" alt="logo"/></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
